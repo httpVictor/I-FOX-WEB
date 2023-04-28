51754136f677607b64ee878f8f75e5dfe44a75e5
DunkArt <pedropaulocezarioribeiro@hotmail.com>
2023-04-28 17:22:12 -0300

    dia 28/04

---
+++ I-FOX-V1/wwwroot/js/flashcards.js
@@ -0,0 +1,27 @@
+﻿
+//Adicionar cards->
+//$(function () {
+
+var imgs
+$('.botaoAzul').on('click', function () {
+
+    var bloco = $('.container:last').clone(),
+        indice = parseInt(bloco.find('span').text()) + 1
+
+    bloco.find('span').text(indice);
+    bloco.find('textarea[name^="frente-"]').attr('name', 'frente-' + indice)
+    bloco.find('textarea[name^="verso-"]').attr('name', 'verso-' + indice)
+    bloco.find('img[id^="img-"]').attr('id', 'img-' + indice)
+    // Se ouver outros inputs, altera o atributo name deles com o índice
+
+    bloco.insertAfter('.container:last')
+    imgs = document.querySelectorAll('.container img')
+    console.log(imgs)
+});
+
+//deletar 
+function deletarCard(card) {
+    var bloco = $(card.remove())
+}
+
+//});
