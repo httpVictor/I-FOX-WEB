cfee4ad2969992f36a92eb757da7036f8b3bf316
VICTOR HUGO NEPOMUCENO <100100636+httpVictor@users.noreply.github.com>
2023-05-31 22:08:11 -0300

    um pouco mais de cards...

--- I-FOX-V1/Models/Mapa.cs
+++ I-FOX-V1/Models/Mapa.cs
@@ -1,9 +1,87 @@
-﻿namespace I_FOX_V1.Models
+﻿using MySql.Data.MySqlClient;
+
+namespace I_FOX_V1.Models
 {
     public class Mapa
     {
         private string usuario;
         private int fases, missoes;
+        static MySqlConnection conexao = FabricaConexao.getConexao();
 
+
+
+
+
+
+        //Método para verificar as missões
+        public void conferirMissoes()
+        {
+
+        }
+
+        //Método para contagem de cadernos
+        public int contaCaderno(string nome)
+        {
+            int qntdCadernos = 0;
+            try
+            {
+                conexao.Open();
+                MySqlCommand select = new MySqlCommand("SELECT * from CADERNO where FK_USUARIO_nome = @usuario", conexao);
+                select.Parameters.AddWithValue("@usuario", nome);
+                MySqlDataReader leitor = select.ExecuteReader();
+
+                while (leitor.Read())
+                {
+                    qntdCadernos++;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return qntdCadernos;
+        }
+
+        //Método para contagem de resumos
+        public int contaResumos(string nome)
+        {
+            int qntdResumos = 0;
+            try
+            {
+                conexao.Open();
+                MySqlCommand select = new MySqlCommand("SELECT * from CADERNO where FK_USUARIO_nome = @usuario", conexao);
+                select.Parameters.AddWithValue("@usuario", nome);
+                MySqlDataReader leitor = select.ExecuteReader();
+
+                while (leitor.Read())
+                {
+                    qntdResumos++;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return qntdResumos;
+        }
+        //Método para contagem de flashcards
+
+        //Método para contagem de resumos escritos
+
+        //Método para contagem de pomodoros
     }
 }
--- I-FOX-V1/wwwroot/js/flashcard_requisicao.js
+++ I-FOX-V1/wwwroot/js/flashcard_requisicao.js
@@ -1,15 +1,46 @@
 ﻿const codigo = $('#codigo').val()
-const codigoofc = parseInt(codigo)
-const paragrafo = $('.escrita p')
-listarCards(codigoofc)
+const codigoLista = parseInt(codigo)
+const frente = $('.front p')
+const verso = $('.back p')
 
-function listarCards(codigooo) {
-    axios.get('../../api/ServicosApi/listar-cartoes', { params: { cod_resumo: codigooo } })
-        .then(response => {
-            const data = response.data
-            paragrafo.text(data)
+
+//variaveis que vão guardar as listas
+var listaFrente
+var listaVerso
+var listaCodigo
+
+//realizando a requisição e guardando em uma variavel
+    listarCards(codigo).then(objeto => {
+        const flashcards = JSON.parse(objeto)
+        listaFrente = flashcards[0].ListaFrente
+        listaVerso = flashcards[0].ListaVerso
+        listaCodigo = flashcards[0].ListaCodigo
+
+        var i = 0
+        frente.text(listaFrente[i])
+        verso.text(listaVerso[i])
+
+        $("#next").on('click', function () {
+            frente.text(listaFrente[i])
+            verso.text(listaVerso[i])
+            i++
         })
-}
 
-//fazendo a classe que vai receber a requisição
-//https://pt.stackoverflow.com/questions/231609/converter-json-em-array-javascript
+    }).catch(error => {
+        console.error(error); // trate erros da requisição
+    });
+
+
+//Requisição para pegar os cards
+function listarCards(codigooo) {
+    return new Promise((resolve, reject) => {
+        axios.get('../../api/ServicosApi/listar-cartoes', { params: { cod_resumo: codigooo } })
+            .then(response => {
+                const lista = response.data;
+                resolve(lista); // resolva a Promise com o objeto retornado
+            })
+            .catch(error => {
+                reject(error); // rejeite a Promise em caso de erro na requisição
+            });
+    });
+}
